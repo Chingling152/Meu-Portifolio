a95f80029134bdd960e82ec246276e3ec829cd2e
Franklin Bitencourt Dantas <47326175810@info.local>
2018-08-30 11:26:33 -0300

    Commit inicial
    Wireframe responsivo

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Pequeno aviso : \nEsse site está incompleto (ainda) ainda tem muitas coisas para arrumar (vou listar as mais obvias):\n-Cor\n-Fonte\n-Nenhum Botão ainda funciona\n-A palavra funciona ali em cima está escrito errado (brincadeira)\n-O Site não tem conteudo , parece até um wireframe em html.");
+    
+    function BarraLateralAnim(){
+        document.getElementById('barra-lateral').classList.toggle('mostra');
+        document.getElementById('botao-barra-lateral').classList.toggle('mostra');
+    }
